a554b1ded528511c2ab3d933c3d769105390f7a1
Sophie Cap <sophie.capricorne@gmail.com>
2020-11-03 14:26:37 +0100

    fixed wrong method call

--- app/src/main/java/com/sophieoc/realestatemanager/view/activity/LoginActivity.kt
+++ app/src/main/java/com/sophieoc/realestatemanager/view/activity/LoginActivity.kt
@@ -27,7 +27,6 @@ class LoginActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        checkLocationEnabled()
         google_sign_in_btn.setOnClickListener { startSignInWithGoogle()}
     }
 

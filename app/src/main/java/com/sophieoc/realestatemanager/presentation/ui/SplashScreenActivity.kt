f6f366853c0cdabd8c4ccb7e00463745a4b0da73
Sophie Cap <sophie.capricorne@gmail.com>
2021-05-26 22:32:51 +0200

    add animation to splash screen

--- app/src/main/java/com/sophieoc/realestatemanager/presentation/ui/SplashScreenActivity.kt
+++ app/src/main/java/com/sophieoc/realestatemanager/presentation/ui/SplashScreenActivity.kt
@@ -1,22 +1,49 @@
 package com.sophieoc.realestatemanager.presentation.ui
 
+import android.os.Build
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.View
+import android.view.WindowInsets
+import android.view.WindowInsetsController
+import android.view.WindowManager
+import android.view.animation.AnimationUtils
+import com.sophieoc.realestatemanager.R
 import com.sophieoc.realestatemanager.databinding.ActivitySplashScreenBinding
 import com.sophieoc.realestatemanager.presentation.BaseActivity
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class SplashScreenActivity : BaseActivity() {
-    lateinit var binding : ActivitySplashScreenBinding
+    lateinit var binding: ActivitySplashScreenBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
-        binding = ActivitySplashScreenBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        if (isCurrentUserLogged())
-            startNewActivity(MainActivity::class.java)
-        else
-            startNewActivity(LoginActivity::class.java)
-        finish()
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            val controller = window.insetsController
+            controller?.systemBarsBehavior = WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+            controller?.hide(WindowInsets.Type.statusBars())
+        } else if (Build.VERSION.SDK_INT < Build.VERSION_CODES.R) {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+
+        binding.appLogo.animation = AnimationUtils.loadAnimation(this, R.anim.splash_screen_anim)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            if (isCurrentUserLogged())
+                startNewActivity(MainActivity::class.java)
+            else
+                startNewActivity(LoginActivity::class.java)
+            finish()
+        }, 5000)
     }
 
-    override fun getLayout() = binding.root
+    override fun getLayout(): View {
+        binding = ActivitySplashScreenBinding.inflate(layoutInflater)
+        return binding.root
+    }
 }
